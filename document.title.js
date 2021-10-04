c398936acaea6ac44c9ac216b58ed1e80577f597
Will Wolff-Myren <willwm@gmail.com>
2021-10-04 13:08:11 -0700

    Prompt user for new document.title value (default to selection or existing document.title)

--- document.title.js
+++ document.title.js
@@ -1,5 +1,5 @@
-// Prompt user for new document.title value
-javascript: (() => { document.title = prompt("document.title?", document.title) || document.title; })();
+// Prompt user for new document.title value (default to selection or existing document.title)
+javascript: (() => { document.title = prompt("document.title?", `${document.getSelection()}` || document.title) || document.title; })();
 
 // Set document.title to selection or textContent of first H1 tag
 javascript: (() => { document.title = `${document.getSelection()}` || document.querySelector('h1').textContent; })();
